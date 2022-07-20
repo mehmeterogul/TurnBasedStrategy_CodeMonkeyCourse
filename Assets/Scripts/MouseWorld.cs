c8f880b030f015bed1d2baa0541b3ead0e0c4234
Mehmet Eroğul <mehmet_erogul@icloud.com>
2022-07-20 23:50:45 +0300

    Initial commit

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private static MouseWorld Instance;
+
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, Instance.mousePlaneLayerMask);
+        return raycastHit.point;
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    [SerializeField] private Animator unitAnimator;
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+        
+        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {   
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 10f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            unitAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("isWalking", false);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
