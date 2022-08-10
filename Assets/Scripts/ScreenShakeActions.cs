01348146303e72ce7ea034f9874b1366e6a14746
Mehmet Eroğul <mehmet_erogul@icloud.com>
2022-08-10 20:39:03 +0300

    sword action

--- Assets/Scripts/ScreenShakeActions.cs
+++ Assets/Scripts/ScreenShakeActions.cs
@@ -10,6 +10,7 @@ public class ScreenShakeActions : MonoBehaviour
     {
         ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
         GrenadeProjectile.OnAnyGrenadeExploded += GrenadeProjectile_OnAnyGrenadeExploded;
+        SwordAction.OnAnySwordHit += SwordAction_OnAnySwordHit;
     }
 
     private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgs e)
@@ -20,5 +21,10 @@ public class ScreenShakeActions : MonoBehaviour
     private void GrenadeProjectile_OnAnyGrenadeExploded(object sender, EventArgs e)
     {
         ScreenShake.Instance.Shake(5f);
+    }
+
+    private void SwordAction_OnAnySwordHit(object sender, EventArgs e)
+    {
+        ScreenShake.Instance.Shake(2f);
     }
 }
--- Assets/Scripts/UnitAnimator.cs
+++ Assets/Scripts/UnitAnimator.cs
@@ -8,6 +8,8 @@ public class UnitAnimator : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Transform bulletProjectilePrefab;
     [SerializeField] private Transform shootPointTransform;
+    [SerializeField] private Transform rifleTransform;
+    [SerializeField] private Transform swordTransform;
 
     private void Awake()
     {
@@ -21,6 +23,17 @@ public class UnitAnimator : MonoBehaviour
         {
             shootAction.OnShoot += ShootAction_OnShoot;
         }
+
+        if(TryGetComponent<SwordAction>(out SwordAction swordAction))
+        {
+            swordAction.OnSwordActionStarted += SwordAction_OnSwordActionStarted;
+            swordAction.OnSwordActionCompleted += SwordAction_OnSwordActionCompleted;
+        }
+    }
+
+    private void Start()
+    {
+        EquipRifle();
     }
 
     private void MoveAction_OnStartMoving(object sender, EventArgs e)
@@ -46,5 +59,28 @@ public class UnitAnimator : MonoBehaviour
         targetUnitAtShootPosition.y = shootPointTransform.position.y;
         
         bulletProjectile.Setup(targetUnitAtShootPosition);
+    }
+
+    private void SwordAction_OnSwordActionStarted(object sender, EventArgs e)
+    {
+        EquipSword();
+        animator.SetTrigger("swordSlash");
+    }
+
+    private void SwordAction_OnSwordActionCompleted(object sender, EventArgs e)
+    {
+        EquipRifle();
+    }
+
+    private void EquipSword()
+    {
+        swordTransform.gameObject.SetActive(true);
+        rifleTransform.gameObject.SetActive(false);
+    }
+
+    private void EquipRifle()
+    {
+        swordTransform.gameObject.SetActive(false);
+        rifleTransform.gameObject.SetActive(true);
     }
 }
