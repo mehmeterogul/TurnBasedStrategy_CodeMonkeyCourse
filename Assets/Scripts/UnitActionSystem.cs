232336cdf5f470fa64ba8ac11f5a4b9580b9ab03
Mehmet Eroğul <mehmet_erogul@icloud.com>
2022-08-11 23:04:48 +0300

    input refactoring

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -75,9 +75,9 @@ public class UnitActionSystem : MonoBehaviour
 
     private bool TryHandleUnitSelection()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(InputManager.Instance.IsMouseButtonDown())
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
 
             if(Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
             {
@@ -106,7 +106,7 @@ public class UnitActionSystem : MonoBehaviour
 
     private void HandleSelectedAction()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(InputManager.Instance.IsMouseButtonDown())
         {
             GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
 
